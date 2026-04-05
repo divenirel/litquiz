4732ee869531efb3fe482dd16f71b3526e3cb557
Your Name <you@example.com>
2026-04-06 00:46:55 +0300

    add question model and admin configuration

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    CATEGORY_CHOICES = [
+        ('author', 'Автор'),
+        ('work', 'Произведение'),
+        ('image', 'По картинке'),
+    ]
+
+    text = models.TextField(verbose_name='Текст вопроса')
+    image = models.URLField(blank=True, null=True, verbose_name='Ссылка на изображение')
+
+    option_a = models.CharField(max_length=255, verbose_name='Вариант A')
+    option_b = models.CharField(max_length=255, verbose_name='Вариант B')
+    option_c = models.CharField(max_length=255, verbose_name='Вариант C')
+    option_d = models.CharField(max_length=255, verbose_name='Вариант D')
+
+    correct_answer = models.CharField(max_length=1, verbose_name='Правильный ответ (A/B/C/D)')
+
+    explanation = models.TextField(blank=True, verbose_name='Пояснение')
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='author')
+
+    def __str__(self):
+        return self.text
